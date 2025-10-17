36ada8842caecd007a6a840af1ba8f3443823739
Marianapadilla1 <marrianagalvan@gmail.com>
2025-10-17 07:47:09 -0600

    Add files via upload
    Practica20

---
+++ practica20/js/index.js
@@ -0,0 +1,27 @@
+let nombre = "Mariana";
+let apellido = " Galván Padilla";
+let nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
+
+
+let numero = 250;
+let numero2 = 300;
+
+var numero3 = 100;
+numero4 =50; // declaraciones antiguas
+
+console.log(nombreCompleto,numero3,numero4);
+
+let suma = numero + numero2 + numero3 + numero4;
+console.log(suma);
+
+console.log(suma);
+
+let resta = numero3 - numero2;
+console.log("El resultado es:", resta);
+
+let multiplica = numero * numero2;
+console.log("El resultado es:", multiplica);
+
+let divide = numero3 / numero4;
+console.log("El resultado de la división es:", divide)
